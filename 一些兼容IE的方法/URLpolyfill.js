8891ce4f6655c94118e5225b6d34aa38c2a1c2f9
贾晓东 <2916331079@qq.com>
2020-05-25 16:10:42 +0800

    阉割版 URLpolyfill

--- 一些兼容IE的方法/URLpolyfill.js
+++ 一些兼容IE的方法/URLpolyfill.js
@@ -1,29 +1,39 @@
-  var URL=URL()||function(str){
-    let output={};
-    var RegExpObj={
-      hash: /.+?(#.+$)/,
-      host: /\w+:\/\/(\S+?)\//,
-      hostname: "/\w+:\/\/([^/:]+)/",
-      href: "http://localhost:8084/tag/getTags?agentId=1107",
-      origin: " /(\w+:\/\/\S+?)\//",
-      password: "",
-      pathname: "/\w+:\/\/\S+?\/(.*)[\?.*]/",
-      port: /\w+:\/\/[^/:]+:?(\d*)?/,
-      protocol: /(\w+:)\/\//,
-      search:  /.+?(\?.+$)/,
-      username: ""
-    }
-    var URLList=['hash','host','hostname','origin','pathname','port','protocol','search']
-    var RexExpHandle=function(patternReg){
-      var pattern=patternReg
-      var arr = str.match(pattern);
-      var outputstr=arr&&arr[1]?arr[1]:"";
-      return outputstr
-    }
-    for(let i=0,len=URLList.length;i<len;i++){
-      output[URLList[i]]=RexExpHandle(RegExpObj[URLList[i]])
-    }
-    RegExpObj['href']=str
-    return output; 
+/**
+ * 
+ * @param {*} str 
+ * 方法比较单一 主要是为了方便获取 URI中的值，跟非IE的浏览器提供的方法差得远
+ */
+
+var URLpolyfill = function(str) {
+  if (new URL(str)) {
+    return new URL(str);
   }
-  export default URL
+  const output = {};
+  var RegExpObj = {
+    hash: /.+?(#.+$)/,
+    host: /\w+:\/\/(\S+?)\//,
+    hostname: "/\w+:\/\/([^/:]+)/",
+    href: "http://localhost:8084/tag/getTags?agentId=1107",
+    origin: " /(\w+:\/\/\S+?)\//",
+    password: "",
+    pathname: "/\w+:\/\/\S+?\/(.*)[\?.*]/",
+    port: /\w+:\/\/[^/:]+:?(\d*)?/,
+    protocol: /(\w+:)\/\//,
+    search: /.+?(\?.+$)/,
+    username: ""
+  }
+  console.log('hhh')
+  var URLList = ['hash', 'host', 'hostname', 'origin', 'pathname', 'port', 'protocol', 'search']
+  var RexExpHandle = function(patternReg) {
+    var pattern = patternReg
+    var arr = str.match(pattern);
+    var outputstr = arr && arr[1] ? arr[1] : "";
+    return outputstr
+  }
+  for (let i = 0, len = URLList.length; i < len; i++) {
+    output[URLList[i]] = RexExpHandle(RegExpObj[URLList[i]])
+  }
+  RegExpObj['href'] = str
+  return output;
+};
+export default URLpolyfill
